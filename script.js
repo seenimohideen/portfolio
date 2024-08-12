1fa08ca076a23e64a828ba9a30bef2b57d6c89ee
CNE <154916219+seenimohideen@users.noreply.github.com>
2024-08-12 23:17:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var sidensav = document.querySelector(".side-nav")
+function shownav(){
+    sidensav.style.left = "0"
+
+
+}
+function closenav(){
+     sidensav.style.left = "-60%"
+
+}
+var skills = document.querySelector(".skills-one")
+function showskill(){
+    skills.style.display = "block"
+}
+function closeskill(){
+    skills.style.display = "none"
+}
+
+var ed = document.querySelector(".ed-one")
+function showed(){
+    ed.style.display = "block"
+}
+function closeed(){
+    ed.style.display = "none"
+}
+
+var pj = document.querySelector(".pj-one")
+function showpj(){
+    pj.style.display = "block"
+}
+function closepj(){
+    pj.style.display = "none"
+}
